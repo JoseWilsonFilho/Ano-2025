43d90da0bf0dce2d9ec227ad8c5e190763a3753f
JoseWilsonFilho <143343680+JoseWilsonFilho@users.noreply.github.com>
2023-08-27 15:17:15 -0300

    Add files via upload

---
+++ Atividade Ano 2025.py
@@ -0,0 +1,31 @@
+print(8*2)
+print('Quanto você ganharia se lavasse 12 carros?')
+print(float(12*12.50))
+print('Quantos anos uma pessoa nascida em 1998 terá em 2025')
+print(2025-1998 , 'anos!')
+print('Quantos anos uma pessoa nascida hoje terá em 2050?')
+print(2050-2023 , 'anos!')
+print('Em que ano você nasceu?')
+nascimento = input()
+nascimento = int(nascimento)
+print('Em 2025 você terá', 2025-nascimento, 'anos!')
+print('Para qual anos quer saber sua idade?')
+x = input()
+x = int(x)
+print('Em', x , 'você terá', x-nascimento, 'anos!')
+print('Qual a sua idade?')
+x = input()
+x = int(x)
+print('Se você fosse um cachorro teria', x*7, 'anos!')
+
+
+
+
+
+
+
+
+
+
+
+
